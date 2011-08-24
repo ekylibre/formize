ee65632d6d9a0704fa336fbc6c9d916cbb73116a
Brice Texier <brice.texier@ekylibre.org>
2011-08-24 11:09:36 +0200

    Try to configure CI with 2 rails environments

---
+++ ci/Gemfile.rails-2.3.rb
@@ -0,0 +1,10 @@
+source "http://rubygems.org"
+
+gem "activesupport", "~> 2.3"
+gem "actionpack", "~> 2.3"
+
+group :development do
+  gem "jeweler", "~> 1.6.4"
+  gem "rcov", ">= 0"
+  gem "rdoc", ">= 2.4.2"
+end
---
+++ ci/Gemfile.rails-3.0.rb
@@ -0,0 +1,10 @@
+source "http://rubygems.org"
+
+gem "activesupport", "~> 3.0"
+gem "actionpack", "~> 3.0"
+
+group :development do
+  gem "jeweler", "~> 1.6.4"
+  gem "rcov", ">= 0"
+  gem "rdoc", ">= 2.4.2"
+end
