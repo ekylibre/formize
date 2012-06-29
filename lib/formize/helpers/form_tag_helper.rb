7d8bee69c30f6538aa71f1d77450272be61e3251
Brice Texier <brice.texier@ekylibre.org>
2012-06-29 11:45:57 +0200

    Fixes bug on non-String name for date_field_tag

--- lib/formize/helpers/form_tag_helper.rb
+++ lib/formize/helpers/form_tag_helper.rb
@@ -11,7 +11,7 @@ module Formize
         end
         format = I18n.translate('date.formats.'+format.to_s)
         Formize::DATE_FORMAT_TOKENS.each{|js, rb| format.gsub!(rb, js)}
-        name_id = name.gsub(/[^a-z]+/, '_')
+        name_id = name.to_s.gsub(/[^a-z]+/, '_')
         html  = ""
         html << hidden_field_tag(name, (value.is_a?(Date) ? value.to_s(:db) : value.to_s), :id => name_id)
         html << tag(:input, :type=>:text, "data-datepicker" => name_id, "data-date-format" => format, :value => localized_value, "data-locale" => ::I18n.locale, :size => options.delete(:size)||10)
